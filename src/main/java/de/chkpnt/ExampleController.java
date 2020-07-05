f6b0d2bce09be231ea793932bc4038d6147c74d2
Gregor Dschung <gregor@chkpnt.de>
2020-07-05 20:20:41 +0200

    Add OpenAPI / Swagger

--- src/main/java/de/chkpnt/ExampleController.java
+++ src/main/java/de/chkpnt/ExampleController.java
@@ -1,11 +1,17 @@
 package de.chkpnt;
 
 import java.io.IOException;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.micronaut.http.annotation.*;
 
 @Controller("/")
+@OpenAPIDefinition(
+        info = @Info(title = "example-service", version = "1.0.0")
+)
 public class ExampleController {
     private static final Logger LOG = LoggerFactory.getLogger(ExampleController.class);
 
